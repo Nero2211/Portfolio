6469cbea1150f3e72f3a25c736faf3c0dedad968
Nero2211 <kartik_Verma@hotmail.com>
2018-03-28 20:44:59 +0100

    further changes almost finished

--- Portfolio HTML/JavaScript/data.js
+++ Portfolio HTML/JavaScript/data.js
@@ -1,7 +1,7 @@
 var sections = [
     {
         subTitle: "Android",
-        subInformation: "Random page patrol is a Wikipedia communal patrol which refers to the process of regularly and frequently checking randomly selected pages through Special:Random. The primary goal of random page patrollers is to ensure random articles conform to Wikipedia's policies on content and style as well as the related guidelines. Secondarily, that style and formatting is consistent and meets the basic principles laid out in Wikipedia's Manual of Style. The guidelines for the New pages patrol and Recent changes patrol also apply, merely the search method is different.",
+        subInformation: "Android is one of my favourite platforms of development because the opportunities for development on Android are endless and limitless. All of this started off which a simple project designed in Eclipse which I was not satisfied with as it was back-end development project with no interface attached. However, the moment I participated in the Mobile Development Unit, it was the turning point. The reason being because I was able to create application on my favourite platform and at that stage, I was filled with numerous application ideas which I would be developing in near future.",
         sectionBackground: "Images/Backgroundimages/work/background1.png",
         slideshowClassname: "Android",
         projects: [
@@ -21,7 +21,7 @@ var sections = [
                 ]
             },
             {
-                title: "UFC Fighter Application",
+                title: "UFC Fighter",
                 sectionGridBackground: "Images/background2.jpg",
                 info: ["Designed using Retrofit Library", "Designed as part of a test for an organisation"],
                 images: [
@@ -39,16 +39,46 @@ var sections = [
                     }
                 ]
             },
+            {
+                title: "Barcode Scanner",
+                sectionGridBackground: "Images/background2.jpg",
+                info: ["Using RESTFUL API Supported by Zoopla", "Designed for the college project"],
+                images: [
+                    {
+                        title: "Splashscreen",
+                        src: "Images/background1.jpg"
+                    },
+                    {
+                        title: "Main search page",
+                        src: "Images/background2.jpg"
+                    }
+                ]
+            },
+            {
+                title: "FixStop",
+                sectionGridBackground: "Images/background2.jpg",
+                info: ["Using RESTFUL API Supported by Zoopla", "Designed for the college project"],
+                images: [
+                    {
+                        title: "Splashscreen",
+                        src: "Images/background1.jpg"
+                    },
+                    {
+                        title: "Main search page",
+                        src: "Images/background2.jpg"
+                    }
+                ]
+            },
         ]
     },
     {
         subTitle: "C#",
-        subInformation: "Random page patrol is a Wikipedia communal patrol which refers to the process of regularly and frequently checking randomly selected pages through Special:Random. The primary goal of random page patrollers is to ensure random articles conform to Wikipedia's policies on content and style as well as the related guidelines. Secondarily, that style and formatting is consistent and meets the basic principles laid out in Wikipedia's Manual of Style. The guidelines for the New pages patrol and Recent changes patrol also apply, merely the search method is different.",
+        subInformation: "C# was the first programming language which was introduced on the course. It all started with simple calculations and looking at different types of variables. However, the moment I started to work with different types of control using Visual Studio, I realised the potential of Visual Studio using C# to create desktop based software. Furthermore, I’ve noticed that C# programming language is also capable of creating simulating user behaviour on computer. Meaning that I can use C# to create automated tools which can be used to eliminate daily tasks. However, this is only a theory as I am yet to practice this simulation process which I will be attending in near future.",
         sectionBackground: "Images/Backgroundimages/work/background2.png",
         slideshowClassname: "CSharp",
         projects: [
             {
-                title: "project 1",
+                title: "Taskco",
                 sectionGridBackground: "Images/background2.jpg",
                 info: ["something with razor Project", "something cool"],
                 images: [
@@ -64,34 +94,17 @@ var sections = [
                     }
                 ]
             },
-            {
-                title: "project 2",
-                sectionGridBackground: "Images/background2.jpg",
-                info: ["potato sim Project", "Something cool"],
-                images: [
-                    {
-                        title: "project 2 image",
-                        src: "Images/background1.jpg",
-                        text: "background 1"
-                    },
-                    {
-                        title: "project 2 image",
-                        src: "Images/background2.jpg",
-                        text: "background 2"
-                    }
-                ]
-            },
         ]
     },
     {
         
-        subTitle: "Unreal Engine",
+        subTitle: "Others",
         subInformation: "Random page patrol is a Wikipedia communal patrol which refers to the process of regularly and frequently checking randomly selected pages through Special:Random. The primary goal of random page patrollers is to ensure random articles conform to Wikipedia's policies on content and style as well as the related guidelines. Secondarily, that style and formatting is consistent and meets the basic principles laid out in Wikipedia's Manual of Style. The guidelines for the New pages patrol and Recent changes patrol also apply, merely the search method is different.",
         sectionBackground: "Images/Backgroundimages/work/background3.png",
         slideshowClassname: "UE",
         projects: [
             {
-                title: "project apples",
+                title: "Adobe Photoshop",
                 sectionGridBackground: "Images/background2.jpg",
                 info: ["game", "something cool about game"],
                 images: [
@@ -108,7 +121,7 @@ var sections = [
                 ]
             },
             {
-                title: "project cupcakes",
+                title: "Unreal Engine",
                 sectionGridBackground: "Images/background2.jpg",
                 info: ["game #2"],
                 images: [
